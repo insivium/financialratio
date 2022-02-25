fdcd19fa8bd6bf16350944ffc6b1e5490cd34e80
insivium <96776048+insivium@users.noreply.github.com>
2022-02-25 16:45:48 +0400

    Update port2.m

--- port2.m
+++ port2.m
@@ -1,4 +1,4 @@
-function port2
+ function port2
   % using the annualized rate formula : (((Final value/Present value)^1/n)-1)
   %where n is the time that the investment is held.
   % port2 will use input functions to calculate the sharpe ratio
@@ -9,27 +9,60 @@ function port2
   % safe_rate will store the value of a risk free security
 
   safe_rate=input('Please enter a risk-free rate in percentage, for example the U.S Treasury Bill ;');
+  while safe_rate<0
+      safe_rate=input('Invalid!Enter a positive integer')
+  end
   per_rate=safe_rate/100;
   total=input('Please enter total investment amount for your first portfolio: ');
+  while total<0
+      total=input('Invalid!Enter a positive integer');
+  end
   a=input('Please enter the duration of your first investment :');
+  while a<0
+      a=input('Invalid!Enter a positive integer');
+  end
   initial_1=input('Please enter the initial investment amount :');
+  while initial_1<0
+      initial_1=input('Invalid!Enter a positive integer');
+  end
   final_1=input('Please enter the final investment amount after appreciation and dividends :');
   b=input('Please enter the duration of your second investment :');
+  while b<0
+      b=input('Invalid!Enter a positive integer');
+  end
   initial_2=input('Please enter the initial investment amount :');
+  while initial_2<0
+      initial_2=input('Invalid!Enter a positive integer');
+  end
   final_2=input('Please enter the final investment amount after appreciation and dividends :');
   c=input('Please enter the duration of your third investment :');
+  while c<0
+      c=input('Invalid!Enter a positive integer');
+  end
   initial_3=input('Please enter the initial investment amount :');
+  while initial_3<0
+      initial_3=input('Invalid!Enter a positive integer');
+  end
   final_3=input('Please enter the final investment amount after appreciation and dividends :');  
   d=input('Please enter the duration of your fourth investment :');
+  while d<0
+      d=input('Invalid!Enter a positive integer');
+  end
   initial_4=input('Please enter the initial investment amount :');
+  while initial_4<0
+      initial_4=input('Invalid!Enter a positive integer');
+  end
   final_4=input('Please enter the final investment amount after appreciation and dividends :');
   e=input('Please enter the duration of your fifth investment :');
+  while e<0
+      e=input('Invalid!Enter a positive integer');
+  end
   initial_5=input('Please enter the initial investment amount :'); 
+  while initial_5<0
+      initial_5=input('Invalid!Enter a positive integer');
+  end
   final_5=input('Please enter the final investment amount after appreciation and dividends :');
-  numbers=[safe_rate;per_rate;total;a;initial_1;final_1;b;initial_2;final_2;c;initial_3;final_3;d;initial_4;final_4;e;initial_5;final_5]
-  while numbers<0
-    disp('Incorrect! Enter positive integer')
-  endwhile
+
   % Calculating annualized return
   % The formula is final return/initial return ^ 1/n-1
   % where n denotes time 
@@ -52,7 +85,7 @@ function port2
   % with respect to the total invested amount
   
   portfolio=(annual_1*weight_1)+(annual_2*weight_2)+(annual_3*weight_3)+(annual_4*weight_4)+(annual_5*weight_5);
-  disp(portfolio)
+  
   
   % Calculating financial ratio : the Sharpe ratio
   % R_p-R_f/standard deviation of return
@@ -75,21 +108,20 @@ function port2
   calc_5=(annual_5)^2-(avg_return)^2;
   variance=calc_1+calc_2+calc_3+calc_4+calc_5;
   n_variance=variance/4;
-  disp(n_variance)
+  
   
   % Calculating standard deviation
   
   std=sqrt(n_variance);
-  disp(std)
   
   % For the Sharpe ratio...
-  # Calculates expected return for every unit of risk taken
+  % Calculates expected return for every unit of risk taken
   % the formula follows
   % return on portfolio-return of a risk free asset/standard deviation
   
   ratio=portfolio-per_rate;
   std_ratio=ratio/std;
-  disp(std_ratio)
+  fprintf('The Sharpe Ratio for the 1st portfolio is %1.3f\n',std_ratio)
   
   % Commentary on the Sharpe ratio
   % A sharpe ratio of <1,proves to be high in risk with low return proportionately
@@ -98,9 +130,9 @@ function port2
   % >1 is considered good
   % >2 considered very good
   % >3 considered excellent
-  if std_ratio<1||std_ratio>0
+  if std_ratio<=0.5
     disp('Suboptimal return for the risk took,it is recommended to invest in an index fund')
-  elseif std_ratio>1
+  elseif std_ratio>0.5
     disp('Good return based on risk taken')
   elseif std_ratio>2
     disp('Very good return,market beating performance')
@@ -108,7 +140,7 @@ function port2
     disp('Excelletent return ,high return for a very low investment risk')
   else
     disp('The sharpe ratio is incorrect,please re-run the program')
-  endif
+  end
 
   % 2nd portfolio calculation
   % All the calculations mentioned in portfolio 1 will be also computed here
@@ -118,22 +150,58 @@ function port2
   % making losses
   
   safe_rate_2=input('Please enter a risk-free rate in percentage, for example the U.S Treasury Bill :');
+  while safe_rate_2<0
+      safe_rate_2=input('Invalid!Enter a positive integer');
+  end
   per_rate_2=safe_rate_2/100;
   total_2=input('Please enter total investment amount for your second portfolio :');
+  while total_2<0
+      total_2=input('Invalid!Enter a positive integer');
+  end
   f=input('Please enter the duration of your first investment for 2nd portfolio :');
+  while f<0
+      f=input('Invalid!Enter a positive integer')
+  end
   initial_6=input('Please enter the initial investment amount :');
+  while initial_6<0
+      initial_6=input('Invalid!Enter a positive integer');
+  end
   final_6=input('Please enter the final investment amount after appreciation and dividends :');
   g=input('Please enter the duration of your second investment for 2nd portfolio:');
+  while g<0
+      g=input('Invalid!Enter a positive integer');
+  end
   initial_7=input('Please enter the initial investment amount :');
+  while initial_7<0
+      initial_7=input('Invalid!Enter a positive integer');
+  end
   final_7=input('Please enter the final investment amount after appreciation and dividends :');
   h=input('Please enter the duration of your third investment for 2nd portfolio :');
+  while h<0
+      h=input('Invalid!Enter a positive integer');
+  end
   initial_8=input('Please enter the initial investment amount :');
+  while initial_8<0
+      initial_8=input('Invalid!Enter a positive integer');
+  end
   final_8=input('Please enter the final investment amount after appreciation and dividends :');  
   i=input('Please enter the duration of your fourth investment for 2nd portfolio:');
+  while i<0
+      i=input('Invalid!Enter a positive integer');
+  end
   initial_9=input('Please enter the initial investment amount :');
+  while initial_9<0
+      initial_9=input('Invalid!Enter a positive integer');
+  end
   final_9=input('Please enter the final investment amount after appreciation and dividends :');
   j=input('Please enter the duration of your fifth investment for 2nd portfolio:');
+  while j<0
+      j=input('Invalid!Enter a positive integer');
+  end
   initial_10=input('Please enter the initial investment amount :'); 
+  while initial_10<0
+      initial_10=input('Invalid!Enter a positive integer');
+  end
   final_10=input('Please enter the final investment amount after appreciation and dividends :');
   
   % Calculating annualized return
@@ -184,7 +252,36 @@ function port2
   
   ratio_2=portfolio_2-per_rate_2;
   std_ratio_2=ratio_2/std_2;
-  disp(std_ratio_2)
+  fprintf('The Sharpe Ratio for the 2nd portfolio is %1.3f\n',std_ratio_2)
+
+  % 2nd portfolio sharpe Ratio commentary
+
+  if std_ratio_2<=0.5
+    disp('Suboptimal return for the risk took,it is recommended to invest in an index fund')
+  elseif std_ratio_2>0.5
+    disp('Good return based on risk taken')
+  elseif std_ratio_2>2
+    disp('Very good return,market beating performance')
+  elseif std_ratio_2>3
+    disp('Excelletent return ,high return for a very low investment risk')
+  else
+    disp('The sharpe ratio is incorrect,please re-run the program')
+  end
+
+  % Sharpe ratios comparison
+
+  if std_ratio_2>std_ratio
+      disp('2nd Portfolio Sharpe ratio is higher')
+  else
+      disp('1st Portfolio Sharpe ratio is higher')
+  end
+
+  % Bar chart graph 
+  x=categorical({'Portfolio 1','Portfolio 2'});
+  y=[std_ratio,std_ratio_2];
+  bar(x,y)
+  title('Sharpe Ratio');
+
 end
 
 
