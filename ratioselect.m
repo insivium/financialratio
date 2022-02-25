f9465e7c7c28f9678ae8ae9f0e5fa63db1e6b81c
insivium <96776048+insivium@users.noreply.github.com>
2022-02-25 16:47:54 +0400

    Update ratioselect.m

--- ratioselect.m
+++ ratioselect.m
@@ -1,16 +1,19 @@
+% function to be called by appselect that display what the user will see
+% in this case different portfolios 
 function choices=ratioselect
   printchoices
   choices=input('');
-  while~any(choices==1:4);
-  disp('Please enter options 1 to 4 .')
+  % take user input 1 to 3
+  while~any(choices==1:3)
+  disp('Please enter options 1 to 3 .')
   printchoices
   choices=input('');
+  end
 end
-
+% statements to be displayed on the menu
 function printchoices
-  fprintf('Please choose the number of portfolios to compare at a time:\n\n')
-  fprintf('1) 2 Portfolios at a time \n')
-  fprintf('2) 3 Portfolios at a time \n')
-  fprintf('3) 4 Portfolios at a time \n')
-  fprintf('4) Exit program \n')
-  
+  fprintf('Please choose the action that needs to be performed:\n\n')
+  fprintf('1) Comparing 2 portfolios Sharpe ratios \n')
+  fprintf('2) Bulk processing calculation of Sharpe ratio using MAT files\n')
+  fprintf('3) Exit program \n')
+end
